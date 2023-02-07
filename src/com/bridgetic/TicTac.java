c1bc1171ae60c7a3e6917d9cc3f29d80ac06f02a
Prajwalbhutey09 <prajwalbhutey09@gmail.com>
2023-02-07 00:51:58 -0800

    code for board

--- src/com/bridgetic/TicTac.java
+++ src/com/bridgetic/TicTac.java
@@ -1,12 +1,14 @@
 package com.bridgetic;
 import java.util.*;
 public class TicTac {
+    static Scanner sc =new Scanner(System.in);
+    static String[] board=new String[10];
         public static void main(String[] args) {
             UC1();
             chooseLetter();
+            UC3showboard();
         }
         public static void UC1(){
-            String [] board = new String[10];
             for(int i = 1; i< board.length; i++){
                 board[i] = String.valueOf(i);
             }
@@ -27,5 +29,13 @@ public class TicTac {
        System.out.println("Invalid Input");
     }
     }
+
+    public static void UC3showboard(){
+        System.out.println("| " +board[1] + " | " +board[2]+ " | " +board[3]+ " |\n-------------");
+        System.out.println("| " +board[4] + " | " +board[5]+ " | " +board[6]+ " |\n-------------");
+        System.out.println("| " +board[7] + " | " +board[8]+ " | " +board[9]+ " |\n-------------");
+    }
+
+
 }
 
